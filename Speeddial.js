3ec87fd1baa9e20c63feca9c83e2b79324708605
John Rayson <j.d.rayson@googlemail.com>
2016-07-03 19:30:12 +0100

    Working * No storage * Needs styling on the dials

--- Speeddial.js
+++ Speeddial.js
@@ -13,6 +13,49 @@ function search()
 	document.location = "https://www.google.co.uk/search?q=" + text;
 }
 
+function drawDial(name, url, colour)
+{
+	var html = "<div class='dial' data-url='" + url + "' style='background-color: " + colour + ";'>"
+		     + "<label>" + name + "</label>"
+		     + "</div>";
+	var $html = $(html);
+	$html.click(function()
+	{
+		document.location = $(this).data("url");
+	});
+	$html.insertBefore("#dial_add");
+}
+
+utils.ready(function()
+{
+	$("#dial_add").click(function()
+	{
+		var id = utils.createUUID();
+		var html = "<label>Name</label><input type='text' id='addText' /><br />"
+				 + "<label>URL</label><input type='text' id='addURL' /><br />"
+				 + "<label>Colour</label><input type='text' id='addColour' />";
+
+		utils.userChoice("Add Dial",html, 
+		{
+			width: 400,
+			buttons: 
+			{ 
+				"Add": function () 
+				{ 
+					drawDial($(this).find("#addText").val(), $(this).find("#addURL").val(), $(this).find("#addColour").val());
+					$(this).empty().remove();
+				},
+				"Close": function () { $(this).empty().remove(); }
+			},
+			open: function()
+			{
+				$(this).find("label").label( { width: "100px" } );
+				$(this).find("input").input();
+			}
+		});
+	});
+});
+
 
 /*
 Add openMyPage() as a listener to clicks on the browser action.
